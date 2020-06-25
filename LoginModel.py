0bed0c7948dcc3b3312863392b080dc1cb80e186
tasim313 <66787305+tasim313@users.noreply.github.com>
2020-06-24 23:56:16 -0400

    Add files via upload

---
+++ LoginModel.py
@@ -0,0 +1,18 @@
+import pymongo,bcrypt
+from pymongo import MongoClient
+class LoginModel:
+    def __init__(self):
+        self.client = MongoClient
+        self.db = self.client.codeWizard
+        self.Users = self.db.users
+
+    def check_user(self,data):
+        user = self.Users.find_one({"username": data.username})
+
+        if user:
+             if bcrypt.checkpw(data.password.encode(),user["password"]):
+                  return user
+             else:
+                 return False
+        else:
+             return False
